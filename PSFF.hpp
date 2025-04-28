637f60253557781084cfa16ca7537f5c2f130ee4
The-bit-wielder <138500792+The-bit-wielder@users.noreply.github.com>
2025-04-28 11:16:44 +0300

    Updated PSFF.hpp
    Refractored code  for readability and efficiency , added value returns on success and failure and improved logic for error handling

--- PSFF.hpp
+++ PSFF.hpp
@@ -1,38 +1,57 @@
-#ifndef PSFF_HPP
+#ifndef PSFF_HPP //Check if library was declared already
 #define PSFF_HPP
-#endif
+#endif //PSFF_HPP
+//Include Standard Libraries
 #include <fstream>
 #include <string>
-const char delimiter = ' ';
-
+//Dont forget to include BDL
+//Declare the namespace
 namespace PSFF {
-	void initializeFile(std::string initialNode, std::string filename, bool shouldOverwrite) {
-		std::string firstLine;
-		if (filename.empty())
+	//Function to initialize the file 
+	bool initializeFile(const std::string& initialNode,const std::string& filename,const bool& shouldOverwrite) {
+		std::string firstLine; //String to store the first line of the file
+		filename = filename.empty() ? "file.psff" : filename;// Check for empty file name and assing a default filename
+		if(initialNode=="") //Check for empty initial node
 		{
-			filename = "file.psff";
-			//bdl
+			//BDL
+			return false;
 		}
-		std::ifstream file(filename);
-		if (file.fail())
+		std::ifstream file(filename); //Open the file for reading
+		if (file.fail()) //Check if file opened succesfuly
 		{
-			//bdl
+			//BDL
+			return false;
 		}
-		else {
-			std::getline(file, firstLine,delimiter);
-			if (initialNode == firstLine) 
+		else { 
+			std::getline(file, firstLine);//Read first line of the file
+			if (initialNode == firstLine) //Check if the file has the declared file name
 			{
-				//bdl
+				//BDL
+				file.close();
+				return true;
 			}
 			else {
-				if (!shouldOverwrite) {
-					//bdl
+				if (!shouldOverwrite) { //Should the program overwrite contents
+					//BDL
+					file.close();
+					return false;
 				}
 				else
 				{
-					//bdl
-					std::ofstream file(filename);
+					//BDL
+					std::ofstream file(filename); //Open file for writing
+					if(!file.fail()) //Check for failure
+					{
 					file <<"[" + initialNode + "]\n";
+					//BDL
+					file.close();
+					return true;
+					}
+					else
+					{
+					//BDL
+					return false;
+					}
 				}
 			}
 		}
